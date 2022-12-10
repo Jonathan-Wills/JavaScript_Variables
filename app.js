c86d47d0e15ce4cf57a3720b3d615c7341c645c5
Jonathan-Wills <wills830@hotmail.com>
2022-12-10 14:33:47 -0500

    initial commit

--- app.js
+++ app.js
@@ -4,7 +4,14 @@ console.log(
 );
 
 // Exercise 1
-
+const firstName = "Jonathan";
+let lastName = "Wills";
+var age = 20;
 // Exercise 2
-
+let fullName = firstName + " " + lastName;
+console.log("fullName : ", fullName);
 // Exercise 3
+let city = "Cincinnati";
+let passtime = "gaming";
+let myStory = `Hello, my name is ${fullName}. I live in ${city}. I enjoy ${passtime} and coding.`;
+console.log(myStory);
